68af610360f2065cf4ee3c5a23a93c0e61dd10bd
chewmann <mattgvo@gmail.com>
2018-10-12 14:34:47 -0600

    commit

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class TopicBrowser extends Component{
+    render() {
+      return(
+        <p>Hello World</p>
+      )
+    }
+  }
+
+  export default TopicBrowser;
